713f46d3d27924963dd4c491bc4e37ea8c5c17e8
salmaQueen <salmawasee9@gmail.com>
2021-01-07 09:42:00 +0300

    tests

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::SessionsController < ApplicationController
+    def create
+
+    end
+
+    def destroy
+
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # devise_for :users
-  resources :sessions, only: [:create, :destroy]
+namespace :api do
+  namespace :v1 do
+    resources :sessions, only: [:create, :destroy]
+  end
+end
+ 
 end
