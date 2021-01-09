af94c76dab1274823365b820b3d4aaa72e575230
salmaQueen <salmawasee9@gmail.com>
2021-01-09 09:57:44 +0300

    heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # devise_for :users
+devise_for :users, skip: :all
 namespace :api do
   namespace :v1 do
-    resources :sessions, only: [:create, :destroy]
+    resources :sessions, only: [:create]
   end
 end
  
