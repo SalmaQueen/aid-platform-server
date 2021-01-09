1e0ada1d5c052ac248a341dddb7743b128aed61c
salmaQueen <salmawasee9@gmail.com>
2021-01-09 09:46:24 +0300

    heroku deployment

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,11 +1,45 @@
 class Api::V1::SessionsController < ApplicationController
-    def create
+    # def create
+    #     user=User.new(user_params)
+    #     if user.save
+    #         token=user.jti
+    #         render json:token
+    #     else
+    #         render json:{errors: user.errors.full_messages}
 
+    # end
+
+  def create
+    already = User
+           .find_by_email(params[:email])
+
+    user=User.new(user_params)
+
+    if already.blank?
+
+
+    if user.save
+      session[:user_id] = user.id
+      render json: {
+        status: :created,
+        logged_in: true,
+        user: user
+      }
+    else
+      render json: { status: 401 }
     end
 
-    def destroy
+else
+render json:{error: "email already in use"}
 
-    end
+end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email,: password)
+  end
 
 
 end
